a9b5d20c60ed02117bc31903e20aa5985d9a8bfb
Todd Pangilinan <pang.todd@gmail.com>
2023-06-06 14:26:12 -0700

    getting warmed up, established first folder for component.

---
+++ vite-project/src/Components/CardTemplate/index.tsx
@@ -0,0 +1,14 @@
+import React, {ReactElement, useEffect, useState} from 'react';
+import '../../index.css';
+
+function CardTemplate() {
+  return(
+    <>
+      <div>
+        <h4>Now what you hear is just a test</h4>
+      </div>
+    </>
+  );
+}
+
+export default CardTemplate;
