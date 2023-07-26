bb9f0ee5736e5bc32068bf05f8a8bedff93355c0
Todd Pangilinan <pang.todd@gmail.com>
2023-07-26 16:36:54 -0700

    established header file, need to flesh it out.

---
+++ vite-project/src/Components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React, {ReactElement, useEffect, useState} from 'react';
+import '../../index.css';
+import {Overlay} from './styles';
+
+function CardTemplate() {
+  return(
+    <Overlay>
+        <h4>Now what you hear is just a test</h4>
+    </Overlay>
+  );
+}
+
+export default CardTemplate;
