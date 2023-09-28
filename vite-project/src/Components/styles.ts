27601cba35ebba05e4be656ab775990b96fa267a
Todd Pangilinan <pang.todd@gmail.com>
2023-09-28 13:38:43 -0700

    created main components stylesheet instead of keeping one in each file. May change structure later.

---
+++ vite-project/src/Components/styles.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import {Card, CardContent} from '@mui/material';
+import '../App.scss';
+
+export const Overlay = styled.div`
+  // height: 90%;
+  // width: 88%;
+  padding: 10%;
+  padding-top: 2.75rem;
+  // position: absolute;
+  // top: 0;
+  // left: 0;
+  color: yellow;
+  text-align: left;
+`;
